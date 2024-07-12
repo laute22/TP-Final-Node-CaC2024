f1e2b8bcecc56de5e09575a9245e127f3c47dbc7
laute22 <162647392+laute22@users.noreply.github.com>
2024-07-11 21:36:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const path = require('path');
+const app = express();
+const puerto = process.env.PORT || 3000; 
+
+// Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+app.use('/api', userRoutes);
+app.listen(puerto, () => {
+  console.log(`Server running on port 3000`); 
+});
